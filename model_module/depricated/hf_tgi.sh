6511812a3646d74b11e04c6036e718f25b30392c
nmorgan <nmorgan@mit.edu>
2026-05-31 00:19:30 +0000

    update codebase

--- model_module/depricated/hf_tgi.sh
+++
@@ -1,9 +0,0 @@
-
-# Set model path to the correct location inside the container
-model=Qwen/Qwen2.5-7B-Instruct""
-
-# Ensure the model directory is mounted correctly
-volume="$PWD/data"
-
-docker run --gpus all --shm-size 1g -p 8080:80 -v $volume:/data \
-    ghcr.io/huggingface/text-generation-inference:latest --model-id $model  --quantize gptq
